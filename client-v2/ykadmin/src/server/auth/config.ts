7699986a19859a3c4aad18b98613a68f609ce634
haianhxanh <hanka7n@gmail.com>
2025-05-02 18:16:08 +0200

    Adjust import aliases

--- client-v2/ykadmin/src/server/auth/config.ts
+++ client-v2/ykadmin/src/server/auth/config.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 
-import { db } from "~/server/db";
+import { db } from "@/server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
