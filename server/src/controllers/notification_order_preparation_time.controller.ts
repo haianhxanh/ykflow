5b8c801d753abfd033906d62735b61dca16bc956
haianhxanh <hanka7n@gmail.com>
2025-07-06 14:24:35 +0200

    Notifications for store's pickup

--- server/src/controllers/notification_order_preparation_time.controller.ts
+++ server/src/controllers/notification_order_preparation_time.controller.ts
@@ -11,14 +11,27 @@ export const notification_order_preparation_time = async (req: Request, res: Res
   try {
     const { orderName, orderEmail } = req.body;
     const content = `
-    <p>Dobrý den,</p>
-    <p>Zvolili jste vyzvednutí Vašich krabiček na odběrném místě <a href="https://maps.app.goo.gl/k2Z3ZyqAfHngWBN79">Cukrááárna - Mratín</a>. Vaše krabičky budou vždy připraveny k vyzvednutí v 16:00.</p>
-    <p>Budeme se na Vás těšit.</p>
-    <br>
-    <p>EN</p>
-    <p>Dear customer ,</p>
-    <p>You have chosen to pick up your meal boxes at the <a href="https://maps.app.goo.gl/k2Z3ZyqAfHngWBN79">Cukrááárna - Mratín</a> pick-up point. Your boxes will always be ready for pickup at 16:00.</p>
-    <p>We look forward to seeing you.</p>
+      <div style="font-family: Arial, sans-serif; color: #000000; background-color: #f7f7f7; padding: 20px; max-width: 600px; margin: auto;">
+      <div style="background-color: #ffffff; padding: 20px; border-radius: 4px; line-height: 1.5;">
+        <p style="margin-top: 0;">Dobrý den,</p>
+        <p>Zvolili jste vyzvednutí Vašich krabiček na odběrném místě 
+          <a href="https://maps.app.goo.gl/k2Z3ZyqAfHngWBN79" style="color: #333333; text-decoration: underline;">Cukrááárna - Mratín</a>. 
+          <br>
+          Vaše krabičky budou vždy připraveny k vyzvednutí v <strong>16:00</strong>.
+        </p>
+        <p>Budeme se na Vás těšit.</p>
+        
+        <hr style="border: none; border-top: 1px solid #d3d3d3; margin: 30px 0;">
+        
+        <p>Dear customer,</p>
+        <p>You have chosen to pick up your meal boxes at the 
+          <a href="https://maps.app.goo.gl/k2Z3ZyqAfHngWBN79" style="color: #333333; text-decoration: underline;">Cukrááárna - Mratín</a> pick-up point.
+          <br>
+          Your boxes will always be ready for pickup at <strong>16:00</strong>.
+        </p>
+        <p>We look forward to seeing you.</p>
+      </div>
+    </div>
     `;
 
     const bbcEmail = MANDRILL_MESSAGE_BCC_ADDRESS_DEV as string | null;
