5314cf61708d3bbb068d01919c48036e299bd867
haianhxanh <hanka7n@gmail.com>
2025-07-29 14:00:55 +0200

    Update routes

--- server/dist/routes/inquiry.route.js
+++ server/dist/routes/inquiry.route.js
@@ -26,6 +26,7 @@ const azure_controller_1 = require("../controllers/azure.controller");
 const custom_email_controller_1 = require("../controllers/custom_email.controller");
 const notification_order_preparation_time_controller_1 = require("../controllers/notification_order_preparation_time.controller");
 const reviews_export_rewards_1 = require("../controllers/reviews_export_rewards");
+const bulk_activate_inventory_at_locations_controller_1 = require("../controllers/bulk_activate_inventory_at_locations.controller");
 const router = express_1.default.Router();
 router.get("/inquiries", authorization_1.auth, get_inquiry_controller_1.get_inquiries);
 router.post("/inquiry/new", receive_inquiry_controller_1.receive_inquiry);
@@ -45,6 +46,7 @@ router.get("/products/export", product_export_for_fakturoid_controller_1.product
 router.get("/reviews/export", reviews_export_1.reviews_export);
 router.get("/reviews/rewards/export", reviews_export_rewards_1.reviews_export_rewards);
 router.get("/products/location/activate", bulk_activate_inventory_at_location_controller_1.bulk_activate_inventory_at_location);
+router.get("/products/locations/activate", bulk_activate_inventory_at_locations_controller_1.bulk_activate_inventory_at_locations);
 router.get("/azure/get-spreadsheet-data", azure_controller_1.getSpreadsheetData);
 router.post("/custom/email", custom_email_controller_1.custom_email);
 // router.post("/easter/discount", getEasterDiscount);
